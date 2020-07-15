2a07b21a6c4941ee2186c05ae94812aab008b58c
kerovinoddi <kerovineoddi@gmail.com>
2020-07-15 18:05:40 +0300

    added index.html

---
+++ config.py
@@ -0,0 +1,27 @@
+ SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 588
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL")
+
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://postgres:risperhub3@localhost/pitch'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
